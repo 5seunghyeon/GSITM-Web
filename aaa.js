27c0e174bac387117455e2c09273cef2e6382554
5seunghyeon <71118117+5seunghyeon@users.noreply.github.com>
2021-08-25 14:49:05 +0900

    Add files via upload

---
+++ aaa.js
@@ -0,0 +1,19 @@
+const button = document.querySelector("button")
+const idInput = document.querySelector("#id")
+const pwInput = document.querySelector("#pw")
+
+
+button.onclick = function(){
+    console.log(`ID : ${idInput.value}, PW : ${pwInput.value}`)
+
+    let id = idInput.value
+    let pw = pwInput.value
+
+    if(localStorage.getItem(id)==null){
+        localStorage.setItem(id,pw)
+        location.href="yang.html"
+        alert("가입되었습니다.")
+    }else{
+        alert(`${id}는 이미 사용중인 아이디입니다.`)
+    }
+}
